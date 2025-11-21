57e3375c2056aa8ecac2ed9a981c591359228c52
Yasir Abdur Rohman <sir.yasirabd@gmail.com>
2025-11-21 20:21:42 +0700

    Update model path and maintain functionality

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ env.allowLocalModels = false;                     // Switch to `true` if you're 
 
 let pipe;
 // UPDATE TO MATCH YOUR MODEL REPOSITORY ON HUGGING FACE HUB
-const modelPath = 'kr15t3n/my-emojigemma-onnx';
+const modelPath = 'yasirabd/myemoji-gemma-3-270m-it-onnx';
 // const modelPath = './modelname-onnx'           // Update if you're using a local model file
 
 // Listen for messages from the main thread
@@ -103,4 +103,5 @@ self.onmessage = async (event) => {
       }
       break;
   }
+
 };
